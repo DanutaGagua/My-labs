020f4a76c7b57b8ff8231e8677e0227d81fb26d4
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2019-12-11 05:26:56 +0300

    Update Main.java

--- ТОКС/Лаба 1/Main.java
+++ ТОКС/Лаба 1/Main.java
@@ -10,7 +10,6 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Main extends JFrame {
-
     private  static JTextArea inputField, outputField, serviceField;
 
     private static String currentInputText = "";
@@ -23,12 +22,11 @@ public class Main extends JFrame {
     String[] dataBitsValues = new String[] {"5", "6", "7", "8"};
     String[] stopBitsValues = new String[] {"1", "1.5", "2"};
     String[] speedValues = new String[] {"50", "75", "100", "150", "300", "600", "1200", "2400", "4800", "9600", "19200",
-                                          "38400", "57600", "115200"};
+            "38400", "57600", "115200"};
 
     private static PortService serialPort;
 
-    public Main(String portName)
-    {
+    public Main(String portName) {
         super(portName);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -38,7 +36,6 @@ public class Main extends JFrame {
     }
 
     private static class PortReader implements SerialPortEventListener {
-
         public void serialEvent(SerialPortEvent event) {
             if (event.isERR()) {
                 serviceField.setText("Debug: incompatible port settings, to fix reconnect to ports with the same params.");
@@ -49,8 +46,7 @@ public class Main extends JFrame {
                 try {
                     String value = new String(serialPort.readByte());
                     outputField.setText(outputField.getText() + value);
-                }
-                catch (NullPointerException ex) {
+                } catch (NullPointerException ex) {
                     System.out.println(ex);
                 }
             }
@@ -68,19 +64,9 @@ public class Main extends JFrame {
         JTextField titleStopBits = createTitle("Stopbits", 35);
         JTextField titleSpeed = createTitle("Speed", 33);
 
-        inputField = new JTextArea(9, 40);
-        inputField.setFont(new Font("Dialog", Font.PLAIN, 14));
-        inputField.setTabSize(10);
-
-        outputField = new JTextArea(9, 40);
-        outputField.setFont(new Font("Dialog", Font.PLAIN, 14));
-        outputField.setTabSize(10);
-        outputField.setEditable(false);
-
-        serviceField = new JTextArea(2, 40);
-        serviceField.setFont(new Font("Dialog", Font.PLAIN, 14));
-        serviceField.setTabSize(10);
-        serviceField.setEditable(false);
+        inputField = getField(9, 40, true);
+        outputField = getField(9, 40, false);
+        serviceField = getField(2, 40, false);
 
         parity = new JComboBox(parityValues);
         parity.setSelectedIndex(0);
@@ -126,115 +112,35 @@ public class Main extends JFrame {
         setSize(500, 700);
         setVisible(true);
 
-        inputField.addKeyListener(new KeyAdapter()
-        {
+        inputField.addKeyListener(new KeyAdapter() {
             @Override
-            public void keyPressed(KeyEvent e)
-            {
-                char symbol = e.getKeyChar();
-                int flag = -1;
-
-                String newText = inputField.getText();
-                if (!currentInputText.equals(newText) && serialPort != null && newText.length() > 0)
-                {
-                    if ((symbol >= ' ' && symbol <= '~') || symbol == '\t' || symbol == '\n')
-                    {
-
-                        if (currentInputText.length() > newText.length())
-                        {
-                            flag = 1;
-                        }
-                        else {
-
-                            for (int  i = 0; i < currentInputText.length(); i++)
-                            {
-                                if (currentInputText.charAt(i) != newText.charAt(i))
-                                {
-                                    flag = i;
-                                    break;
-                                }
-                            }
-
-                            if (flag == -1) {
-                                char value = newText.charAt(newText.length() - 1);
-
-                                serialPort.writeByte((byte) value);
-                                
-                                currentInputText = newText;
-                            }}
-                    }
-                    else
-                    {
-                        flag = 0;
-                    }
-
-                    if (flag > -1)
-                    {
-                        inputField.setText(currentInputText);
-                        serviceField.setText("You can add only to end string and only standart ascii characters.");
-                    }
-                }
+            public void keyPressed(KeyEvent e) {
+                processKey(e);
             }
 
             @Override
-            public void keyReleased(KeyEvent e)
-            {
-                char symbol = e.getKeyChar();
-
-                int flag = -1;
-
-                String newText = inputField.getText();
-                if (!currentInputText.equals(newText) && serialPort != null && newText.length() > 0)
-                {
-                    if ((symbol >= ' ' && symbol <= '~') || symbol == '\t' || symbol == '\n')
-                    {
-
-                        if (currentInputText.length() > newText.length())
-                        {
-                            flag = 1;
-                        }
-                        else {
-
-                        for (int  i = 0; i < currentInputText.length(); i++)
-                        {
-                            if (currentInputText.charAt(i) != newText.charAt(i))
-                            {
-                                flag = i;
-                                break;
-                            }
-                        }
-
-                        if (flag == -1) {
-                            char value = newText.charAt(newText.length() - 1);
-
-                            serialPort.writeByte((byte) value);
-
-                            currentInputText = newText;
-                        }}
-                    }
-                    else
-                    {
-                        flag = 0;
-                    }
-
-                    if (flag > -1)
-                    {
-                        inputField.setText(currentInputText);
-                        serviceField.setText("You can add only to end string and only standart ascii characters.");
-                    }
-                }
+            public void keyReleased(KeyEvent e) {
+                processKey(e);
             }
         });
     }
 
-    public JTextField createTitle(String name, int columns)
-    {
+    public JTextField createTitle(String name, int columns) {
         JTextField title = new JTextField(name);
         title.setFont(new Font("Dialog", Font.PLAIN, 14));
         title.setColumns(columns);
         title.setEditable(false);
 
         return title;
+    }
+
+    public JTextArea getField(int rows, int columns, boolean isEditable) {
+        JTextArea field = new JTextArea(rows, columns);
+        field.setFont(new Font("Dialog", Font.PLAIN, 14));
+        field.setTabSize(10);
+        field.setEditable(isEditable);
+
+        return field;
     }
 
     private void connect(String portName) {
@@ -258,17 +164,15 @@ public class Main extends JFrame {
         if ((dataBits == SerialPort.DATABITS_5) && (stopBits == SerialPort.STOPBITS_2)) flag = true;
         if ((dataBits == SerialPort.DATABITS_6) && (stopBits == SerialPort.STOPBITS_1_5)) flag = true;
         if ((dataBits == SerialPort.DATABITS_7 ) && (stopBits == SerialPort.STOPBITS_1_5)) flag = true;
-        if ((dataBits == SerialPort.DATABITS_8 ) && (stopBits == SerialPort.STOPBITS_1_5)) flag = true;//}
-        if (flag)
-        {
+        if ((dataBits == SerialPort.DATABITS_8 ) && (stopBits == SerialPort.STOPBITS_1_5)) flag = true;
 
+        if (flag) {
             serviceField.setText("Incorrect params, can't connect.");
             serialPort.close();
             return;
         }
 
         serialPort.setParams(parity, dataBits, stopBits, speed);
-
         serialPort.setFlowControl(SerialPort.FLOWCONTROL_RTSCTS_IN | SerialPort.FLOWCONTROL_RTSCTS_OUT);
         serialPort.addListener(new PortReader());
 
@@ -285,13 +189,20 @@ public class Main extends JFrame {
 
             int value = -1;
 
-            if (data.equals("5")) value = SerialPort.DATABITS_5;
-            if (data.equals("7")) value = SerialPort.DATABITS_7;
-            if (data.equals("6")) value = SerialPort.DATABITS_6;
-            if (data.equals("8")) value = SerialPort.DATABITS_8;
+            if (data.equals("5")) {
+                value = SerialPort.DATABITS_5;
+            }
+            if (data.equals("7")) {
+                value = SerialPort.DATABITS_7;
+            }
+            if (data.equals("6")) {
+                value = SerialPort.DATABITS_6;
+            }
+            if (data.equals("8")) {
+                value = SerialPort.DATABITS_8;
+            }
 
             return value;
-
         } catch (NullPointerException e) {
             return -1;
         }
@@ -309,6 +220,7 @@ public class Main extends JFrame {
         try {
             int value;
             String s = parity.getSelectedItem().toString();
+
             if (s.equals("EVEN")) {
                 value = SerialPort.PARITY_EVEN;
             }
@@ -325,7 +237,6 @@ public class Main extends JFrame {
             }
 
             return value;
-
         } catch (NullPointerException e) {
             return -1;
         }
@@ -335,30 +246,63 @@ public class Main extends JFrame {
         try {
             int value = 0;
             String s = stopBits.getSelectedItem().toString();
+
             if (s.equals("1")) {
                 value = SerialPort.STOPBITS_1;
-            }else {
+            }
             if (s.equals("1.5")) {
                 value = SerialPort.STOPBITS_1_5;
-            } else {
-                if (s.equals("2"))
+            }
+            if (s.equals("2")){
                 value = SerialPort.STOPBITS_2;
-            }}
+            }
 
             return value;
-
         } catch (NullPointerException e) {
             return -1;
         }
     }
 
-    public static void main(String[] args)
-    {
-        if (args.length > 0) {
-            PortService port = new PortService(args[0]);
-            new Main(args[0]);
+    private void processKey(KeyEvent e){
+        char symbol = e.getKeyChar();
+
+        String newText = inputField.getText();
+        if (!currentInputText.equals(newText) && serialPort != null && newText.length() > 0) {
+            if (characterIsCorrectlyEntered(newText, symbol, currentInputText)) {
+                char value = newText.charAt(newText.length() - 1);
+                serialPort.writeByte((byte) value);
+                currentInputText = newText;
+            } else {
+                inputField.setText(currentInputText);
+                serviceField.setText("You can add only to end string and only standart ascii characters.");
+            }
         }
-        else {
+    }
+
+    public boolean characterIsCorrectlyEntered(String newText,
+                                               char character,
+                                               String currentInputText) {
+        if ( !((character >= ' ' && character <= '~') || character == '\t' || character == '\n') ) {
+            return false;
+        }
+
+        if (currentInputText.length() > newText.length()) {
+            return false;
+        }
+
+        for (int  i = 0; i < currentInputText.length(); i++) {
+            if (currentInputText.charAt(i) != newText.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            new Main(args[0]);
+        } else {
             System.out.println("Didn't found name of port.");
         }
     }
