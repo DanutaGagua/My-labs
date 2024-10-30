ecaf89364c6b4136537d6553315dba382d425f67
DanutaGagua <49496698+DanutaGagua@users.noreply.github.com>
2024-10-30 12:19:36 +0300

    Add lab1 for Windows

---
+++ СПОВМ/Lab 1/Windows/Archive_system.cpp
@@ -0,0 +1,71 @@
+﻿//#include "pch.h"
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+//#include <Windows.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{	
+	if (argc < 3)
+	{
+		cout << endl << "Not enough arguments." << endl;
+
+		return -2;
+	}
+	
+	fstream file;
+
+	file.open("input.txt", ios::in);
+
+	if (!file)
+	{
+		cout << "\nFile doesn't exist.\n";
+
+		return -1;
+	}
+
+	int raws = 0;
+	vector <string> data;
+
+	while (!file.eof())
+	{
+		char str[1000];
+
+		file.getline(str, 1000);
+
+		data.push_back(string(str));
+
+		//cout << str << endl;
+
+		raws++;
+	}
+	
+	for (int j = 1; j < argc; j++)
+	{
+		int flag = 1;
+
+		for (vector<string>::iterator i = data.begin(); i != data.end(); i++)
+		{
+			//cout << *i << endl;
+
+			if ((*i).find(argv[j]) != string::npos)
+			{
+				cout << flag << " " << (*i) << endl;
+			}
+			
+			flag++;
+		}
+	}
+
+	file.close();
+
+	getchar();
+
+	//system("pause");
+
+	return 0;
+}
---
+++ СПОВМ/Lab 1/Windows/лаба1 СПОВМ.cpp
@@ -0,0 +1,77 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+//#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	int number;
+
+	cout << "Enter number raws: ";
+
+	cin >> number;
+
+	char data[200];
+
+	data[0] = '\0';
+
+	cin.ignore(1);
+
+	for (int i = 0; i < number; i++)
+	{
+		char str[200];
+
+		cin.getline(str, 200);
+		//cin.ignore(1);
+
+		strcat_s(data, 200, str);
+		strcat_s(data, 200, " ");
+	}
+	//cin.ignore(1);
+
+	string str = "Archive_system.exe ";
+	str += data;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	if (!CreateProcess(NULL,					// No module name (use command line)
+		(char *)str.data(),
+		NULL,					// Process handle not inheritable
+		NULL,					// Thread handle not inheritable
+		FALSE,					// Set handle inheritance to FALSE
+		0,		// lol
+		NULL,					// Use parent's environment block
+		NULL,					// Use parent's starting directory 
+		&si,					// Pointer to STARTUPINFO structure
+		&pi)					// Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		cout << "CreateProcess failed.\n";
+		return -1;
+	}
+
+	SYSTEMTIME lt;
+
+	while (WaitForSingleObject(pi.hProcess, 100))
+	{
+		GetLocalTime(&lt);
+		cout << lt.wHour << "." << lt.wMinute << "." << lt.wSecond << "\r";
+	}
+	
+	CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+	
+	return 0;
+}
+
+
